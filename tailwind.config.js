fc258639e17ebac6e4ede7dbfec7d7e254221b35
SUZANE ARCANJO DA HORA <23111828@CFP127ALUNO.LOCAL>
2024-05-21 12:28:15 -0300

    em andamento

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'imageFundo' : "url('./img/imgFundo.svg')"
+        'Fundo' : "url('./img/fundo.svg')"
       }
     },
   },
